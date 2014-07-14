8a552b580c93e63a27b900bae1d6d67e7acdf740
Hong Jeon <hongj77@gmail.com>
2014-07-14 16:59:23 -0500

    jquery responsive image size

--- assets/js/scriptin.js
+++ assets/js/scriptin.js
@@ -1,7 +1,19 @@
 
 $(function() {
-	var winHeight = $(window).height();
-	// $('#header').css('height', winHeight);
-	console.log("header's height is now ", winHeight);
+	var $window = $(window);
+
+	var picWidth = $('#pic').width();
+	$('#profile-pic').attr({'width': picWidth, 'height': picWidth});
+
+
+	$window.scroll(function (event) {
+		var scrollTop = $window.scrollTop();
+
+		console.log(scrollTop);
+		if (scrollTop*0.4 < 250) {
+			$('#box1').animate({height: scrollTop*0.4, width: scrollTop*0.4}, 10);
+		}
+		
+	});
 
 }); //on load 
