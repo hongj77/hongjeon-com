be3509391b8b62ec1f71be5e1d65b785f0113ebd
Hong <hongj77@gmail.com>
2014-07-15 00:04:45 -0500

    main layout established

--- assets/js/scriptin.js
+++ assets/js/scriptin.js
@@ -2,17 +2,19 @@
 $(function() {
 	var $window = $(window);
 
-	var picWidth = $('#pic').width();
-	$('#profile-pic').attr({'width': picWidth, 'height': picWidth});
+	// var picWidth = $('#pic').width();
+	// $('#profile-pic').attr({'width': picWidth, 'height': picWidth});
 
 
 	$window.scroll(function (event) {
 		var scrollTop = $window.scrollTop();
 
 		console.log(scrollTop);
-		if (scrollTop*0.4 < 250) {
-			$('#box1').animate({height: scrollTop*0.4, width: scrollTop*0.4}, 10);
-		}
+		// if (scrollTop*0.5 < 250) {
+		// 	$('#box1').css({height: scrollTop*0.5, width: scrollTop*0.5});
+		// }
+
+		
 		
 	});
 
