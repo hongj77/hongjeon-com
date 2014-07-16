c5ee69d01a758c5aea8559b53e9bca2dc7c7ad8f
Hong <hongj77@gmail.com>
2014-07-16 17:06:43 -0500

    more jquery, refined layout, more content

--- assets/js/scriptin.js
+++ assets/js/scriptin.js
@@ -2,14 +2,20 @@
 $(function() {
 	var $window = $(window);
 
-	// var picWidth = $('#pic').width();
-	// $('#profile-pic').attr({'width': picWidth, 'height': picWidth});
-
-
+	//scroll responsive
 	$window.scroll(function (event) {
 		var scrollTop = $window.scrollTop();
 
 		console.log(scrollTop);
+
+		if (scrollTop < 420) {
+			makeIdDissapear('#me-1');
+			makeIdDissapear('#me-2');
+			makeIdDissapear('#me-3');
+		}
+		// if (scrollTop < 550) {
+		// 	makeIdDissapear('#project-section');
+		// }
 		if (scrollTop > 690) {
 			makeIdAppear('#me-1');
 		}
@@ -20,11 +26,46 @@ $(function() {
 			makeIdAppear('#me-3');
 		}
 
+		// if (scrollTop > 1250) {
+		// 	makeIdAppear('#project-section');
+		// }
 	});
 
+	//hover responsive
+	faded('#me-1');
+	faded('#me-2');
+	faded('#me-3');
+
 }); //on load 
+
+function faded (id) {
+	var idHead = id + '-head';
+	var idText = id + '-text';
+	var idDark = id + '-dark';
+
+	$(id).mouseenter(function() {
+		$(idDark).css('visibility', 'visible');
+		$(idHead).css('visibility', 'visible');
+		$(idHead).removeClass('fadeOut');
+		$(idHead).addClass('animated fadeIn');
+		$(idText).css('visibility', 'visible');
+		$(idText).removeClass('fadeOutDown');
+		$(idText).addClass('animated fadeInUp');
+	}).mouseleave(function() {
+		$(idDark).css('visibility', 'hidden');
+		$(idHead).removeClass('fadeIn');
+		$(idHead).addClass('fadeOut');
+		$(idText).removeClass('fadeInUp');
+		$(idText).addClass('fadeOutDown');
+	});
+}
 
 function makeIdAppear (id) {
 	$(id).css('visibility', 'visible');
 	$(id).addClass("animated fadeIn");
+}
+
+function makeIdDissapear(id) {
+	$(id).css('visibility', 'hidden');
+	$(id).removeClass("animated fadeIn");
 }
